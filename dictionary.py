873dbd2816c70b93881ff8d48044b7eb987b3a04
Travis Gautier <tgautier13@gmail.com>
2018-05-28 10:33:21 -0500

    Complete program on the command line.

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,41 @@
+##Interactive Dictionary 
+##Travis Gautier 2018
+##
+##
 import json
-from difflib import get_close_matches
+from difflib import SequenceMatcher         # similiarity ratio library for detecting close matches
+from difflib import get_close_matches       # finding the best match of a word
 
 data = json.load(open("data.json"))
 
 def translate(w):
-    if w in data:
+    w = w.lower()       # making user in put case-insensitive
+    if w in data:       # checks if user input is a word or not
         return data[w]
+    elif w.title() in data:                             # if the user entered "kansas" instead of "Kansas"
+        return data[w.title()]
+    elif w.upper() in data:                             # if the user enters an acronym
+        return data[w.upper()]
+    elif len(get_close_matches(w, data.keys())) > 0:    # checks if user input is a word/spelled correctly
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]               # if suggested word is correct
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."        # if suggested word is incorrect
+        else:
+            return "We didn't understand your entry."                       # if the user did not type y or n
     else:
         return "The word doesn't exist. Please double check it."
 
-word = input("Enter word: ")
+word = input("Enter word: ")        # ask for user input
 
-print(translate(word))
+output = translate(word)
+if type(output) == list:            # making sure the user input is a list, and not a string for example
+    for item in output:
+        print(item)
+else:
+    print(output)
+
+
+
+
